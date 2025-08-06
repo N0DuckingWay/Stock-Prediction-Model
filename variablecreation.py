3e3d3bc5ababa5c21429d3cacb6563fe7207e85a
Zachary Hoffman <zdhoffman@gmail.com>
2025-08-06 03:44:06 -0700

    added functionality to get values for non-us companies using certain currencies

--- variablecreation.py
+++ variablecreation.py
@@ -16,6 +16,11 @@ fredkey = '***REMOVED***'
 
 #%%
 allfinancials = pd.read_pickle('Data/allfinancials.p')
+#%%
+
+##Only here because getfinancials.py does not have the ability to convert currencies yet.
+
+allfinancials = allfinancials.loc[allfinancials.currency == 'USD']
 
 #%% #Adding in BLS data
 headers = {'Content-type': 'application/json'}
