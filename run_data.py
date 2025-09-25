975c401c7100852ff61d3dd6c6b133bd5a3d756a
Zachary Hoffman <zdhoffman@gmail.com>
2025-09-25 03:17:51 +0000

    changes made to improve data quality.

--- run_data.py
+++ run_data.py
@@ -4,9 +4,13 @@ Created on Wed Aug 13 08:56:19 2025
 
 @author: Paperspace
 """
-import gc
+import gc, os
+os.chdir(r'C:\Users\paperspace\Documents\Coding Projects\Stock-Prediction-Model')
 import datagathering
 gc.collect()
 import variablecreation
 gc.collect()
 import data_transformations
+gc.collect()
+import sfa
+gc.collect()
