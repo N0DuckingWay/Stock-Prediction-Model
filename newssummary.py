e3dcf977d87d4b252c58e8cfe98880e7f7fbecc7
Zachary Hoffman <zdhoffman@gmail.com>
2025-11-04 01:35:46 +0000

    fixed prompt issues, prompt now showing consistent results from the looks of it.

--- newssummary.py
+++ newssummary.py
@@ -46,29 +46,68 @@ for h in range(len(tickers)):
                 Use web search to summarize the news for ticker symbol {t} on every one of the following dates: {subdates}. For each date, rate the news on that date, as well as in the 7 days leading up to
                 that date, 31 days leading up to that date, 90 days leading up to that date, and 365 days leading up to that date.
                 The news should be rated on a 0-10 scale, with 0 being most negative, 5 being neutral, and 10 being most positive.
-                Days with no news should have a null value. Results should be formatted as a comma separated table with "date",
-                "rating_onday","rating_lastsevendays",rating_last31days", "rating_last90days", "rating_last365days", and "sources", and "notes" as the columns.
+                Days with no news should have a null value. Results should be formatted as a json with "date",
+                "rating_onday","rating_lastsevendays",rating_last31days", "rating_last90days", "rating_last365days", and "sources", and "notes" as properties.
                 "date" should only contain the date, "rating_onday" should only contain the integer rating of the news on that date only. "rating_lastsevendays" should only contain the integer rating of the news over the seven days leading up to that date.
                 "rating_last31days" should only contain the integer rating of the news over the 31 days leading up to that date, "rating_last90days" should only contain the integer rating of the news over the last 90 days leading up to that date.
                 "rating_last365days" should only contain the integer rating of the news over the last 365 days leading up to that date. "sources" contains a list of sources, separated by semicolons (do not use commas).
                 "notes" contains any notes.
                 
-                Do not ask follow up questions. Use only the information provided. Only return the table. Do not return any notes or text outside the table.''',
+                Do not ask follow up questions. Use only the information provided. Only return the JSON.''',
                 tools=[{'type':'web_search'}])
-                outtext = response.output_text
-                print(outtext)
-                csv_text = StringIO(outtext)
-                csv = pd.read_csv(csv_text,on_bad_lines='skip')
-                if 'date' not in csv.columns or 'rating_onday' not in csv.columns or "rating_lastsevendays" not in csv.columns or "rating_last31days" not in csv.columns or "rating_last90days" not in csv.columns or "rating_last365days" not in csv.columns:
+                text={
+                    'format':{
+                        'type':'json_schema',
+                        'name':'ratings',
+                        'strict':True,
+                        'schema':{
+                            'type':'object',
+                            'properties':{
+                                'date': {
+                                    'type':'string'},
+                                'rating_onday':{
+                                    'type': 'number',
+                                    'minimum':0,
+                                    'maximum':10},
+                                'rating_lastsevendays':{
+                                    'type': 'number',
+                                    'minimum':0,
+                                    'maximum':10},
+                                'rating_last31days':{
+                                    'type': 'number',
+                                    'minimum':0,
+                                    'maximum':10},
+                                'rating_last90days':{
+                                    'type': 'number',
+                                    'minimum':0,
+                                    'maximum':10},
+                                'rating_last365days':{
+                                    'type': 'number',
+                                    'minimum':0,
+                                    'maximum':10},
+                                'sources':{
+                                    'type':'string'},
+                                'notes':{
+                                    'type':'string'}
+                                },
+                            'required':[
+                                'date','rating_onday','rating_lastsevendays','rating_last31days','rating_last90days','rating_last365days','sources','notes'],
+                            'additionalproperties':False
+                            }}}
+                outjson = response.output_text
+                
+                
+                resultdf = pd.read_json(outjson)
+                if 'date' not in resultdf.columns or 'rating_onday' not in resultdf.columns or "rating_lastsevendays" not in resultdf.columns or "rating_last31days" not in resultdf.columns or "rating_last90days" not in resultdf.columns or "rating_last365days" not in resultdf.columns:
                     raise ValueError(f'error pulling data for {t}. Check outtext variable for details.')
                 else:
-                    ratingcols = [x for x in csv if 'rating' in x]
-                    csv[ratingcols] = csv[ratingcols].astype(float)
-                if len(list(set(csv.dtypes[ratingcols]))) != 1 and list(csv.dtypes[ratingcols])[0] != float:
+                    ratingcols = [x for x in resultdf if 'rating' in x]
+                    resultdf[ratingcols] = resultdf[ratingcols].astype(float)
+                if len(list(set(resultdf.dtypes[ratingcols]))) != 1 and list(resultdf.dtypes[ratingcols])[0] != float:
                     raise ValueError(f'error pulling data for {t}. Check outtext variable for details.')
                 
                 else:
-                    tickerdata = pd.concat([tickerdata,csv],axis=0)
+                    tickerdata = pd.concat([tickerdata,resultdf],axis=0)
                     completed.append(t)
                     print(f'Succesfully pulled data for rows {i} to {i+20} of {t}. Roughly {round(100*(h+1)/len(tickers),2)}% finished.')
                     success = 1
@@ -76,7 +115,7 @@ for h in range(len(tickers)):
                 if tries <maxtries-1:
                     tries +=1
                 else:
-                    errortickers[t] = {'error':e,'response':outtext}
+                    errortickers[t] = {'error':e,'response':outjson}
                     print(f'Failed to pull data for rows {i} to {i+20} of  {t}. Roughly {round(100*(h+1)/len(tickers),2)}% finished.')
                     tries += 1
             print(f'Completed {i} rows of {t}')
